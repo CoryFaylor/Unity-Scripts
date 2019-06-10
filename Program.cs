eca263ea815e0386eb16171bcf8fb1c002f4ada8
CoryFaylor <51675343+CoryFaylor@users.noreply.github.com>
2019-06-10 14:11:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (other.tag == "Player")
+            other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    private void OnTriggerStay2D(Collider other)
+    {
+        if (!(other.tag == "Player"))
+            return;
+
+        float y = Input.GetAxis("Vertical");
+        other.transform.Translate(new Vector3(0, other, 0));
+    }
+    private void OnTriggerExit2D(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
