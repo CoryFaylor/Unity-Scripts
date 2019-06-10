08addc6d842dc7ff33d47a2dd87dbf40bb674b1c
CoryFaylor <51675343+CoryFaylor@users.noreply.github.com>
2019-06-10 14:56:28 -0400

    Add files via upload

---
+++ OneWayPlatforms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWayPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Put this in the Update
+            //Added One Way Platform
+            if (m_Grounded == true && gameObject.layer != 8)
+                gameObject.layer = 8;
+
+
+            if (m_Grounded == true && Input.GetKey(KeyCode.S))
+            {
+                gameObject.layer = 10;
+            }
+        }
+    }
+}
